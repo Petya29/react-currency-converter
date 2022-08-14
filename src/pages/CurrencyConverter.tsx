705586acd7b7161f72f7b09c7bbaff09eb2b1915
Petya29 <55765677+Petya29@users.noreply.github.com>
2022-08-14 19:09:14 +0200

    init

---
+++ src/pages/CurrencyConverter.tsx
@@ -0,0 +1,9 @@
+import { FC } from 'react';
+
+export const CurrencyConverter: FC = () => {
+  return (
+    <h1 className="text-3xl font-bold underline">
+      Hello Vite + React + Tailwind
+    </h1>
+  )
+}
