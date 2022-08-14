cecd39e2716eeb42ed4c1e912c246e923f26c975
Petya29 <55765677+Petya29@users.noreply.github.com>
2022-08-15 00:11:50 +0200

    Implement convertation logic

--- src/helpers/currency.ts
+++ src/helpers/currency.ts
@@ -2,13 +2,17 @@ import { IAPI } from "../models/IApi";
 import { ICurrencies } from "../models/ICurrencies";
 
 export const formatExchangeRates = (currencies: IAPI[]): string => {
-    const USD = currencies.findIndex(curr => curr.ccy === 'USD');
-    const EUR = currencies.findIndex(curr => curr.ccy === 'EUR');
+    try {
+        const USD = currencies.findIndex(curr => curr.ccy === 'USD');
+        const EUR = currencies.findIndex(curr => curr.ccy === 'EUR');
 
-    const USDtoString = `USD ${Number(currencies[USD].buy).toFixed(1)}/${Number(currencies[USD].sale).toFixed(1)}`;
-    const EURtoString = `EUR ${Number(currencies[EUR].buy).toFixed(1)}/${Number(currencies[EUR].sale).toFixed(1)}`;
+        const USDtoString = `USD ${Number(currencies[USD].buy).toFixed(1)}/${Number(currencies[USD].sale).toFixed(1)}`;
+        const EURtoString = `EUR ${Number(currencies[EUR].buy).toFixed(1)}/${Number(currencies[EUR].sale).toFixed(1)}`;
 
-    return `${USDtoString} ${EURtoString}`;
+        return `${USDtoString} ${EURtoString}`;
+    } catch (err) {
+        return '';
+    }
 }
 
 export const formatCurrencies = (api: IAPI[]): ICurrencies => {
@@ -32,4 +36,26 @@ export const formatCurrencies = (api: IAPI[]): ICurrencies => {
     }
 
     return currencies;
+}
+
+export const doubleConvert = (from: string, to: string, sum: string, currencies: ICurrencies): string => {
+    for (const currency in currencies) {
+        const iterableCurr = currencies[currency];
+        if (iterableCurr.hasOwnProperty(from) && iterableCurr.hasOwnProperty(to)) {
+            const firstConvert = +sum * +currencies[from][currency];
+            return String(firstConvert * +iterableCurr[to]);
+        }
+    }
+
+    return 'Triple conversion is not supported';
+}
+
+export const convert = (from: string, to: string, sum: string, currencies: ICurrencies): string => {
+    if (from === to) return sum;
+
+    if (!currencies.hasOwnProperty(from) || !currencies[from].hasOwnProperty(to)) {
+        return 'double convert';
+    }
+
+    return String(+sum * +currencies[from][to]);
 }
